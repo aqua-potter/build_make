6f7e24f53ce053542617e6ee7e43b84a92fed45a
Khalid Zubair <kzubair@cyngn.com>
2016-03-03 11:45:58 -0800

    kernel: add docs for config vars
    Add a header with documentation for the config vars currently in use
    by the Kernel build.
    
    Change-Id: I6930b41d716baabbb16c2f407b6b9fe6e5f2b4c8
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>

--- core/tasks/kernel.mk
+++ core/tasks/kernel.mk
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+
 # Android makefile to build kernel as a part of Android Build
 #
 # Configuration
